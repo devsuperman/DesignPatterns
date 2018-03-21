31928fdda86607eb02761f49ecc837a71cba085c
Tiago Henrique dos Santos Bernardo <01407634224@rondonia.local>
2018-03-21 16:42:49 -0400

    Factory Pronto

---
+++ Factory/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            var conexao = new SqlConnectionFactory().Criar();
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class SqlConnection
+    {
+        public string ConnectionString { get; set; }             
+
+        public void AbrirConexao()
+        {
+
+        }
+    }
+
+    public class SqlConnectionFactory
+    {
+        public SqlConnection Criar()
+        {
+            var conexao = new SqlConnection();
+            conexao.ConnectionString = "User Id=root;Password=;Server=localhost;Database=banco";
+            conexao.AbrirConexao();
+            return conexao;
+        }
+    }
+}
